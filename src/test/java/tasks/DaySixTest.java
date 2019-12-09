69be6a1abc08a8eebff624817b2558836e7ebfe8
Brainslugger <markus.alvermann@web.de>
2019-12-09 05:22:17 +0100

    Day Six partial: added input and class structure

---
+++ src/test/java/tasks/DaySixTest.java
@@ -0,0 +1,25 @@
+package tasks;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DaySixTest {
+
+    DaySix daySix;
+
+    @BeforeEach
+    void setUp() {
+        daySix = new DaySix();
+    }
+
+    @Test
+    void testsPlanetBOrbitsPlanetA() {
+
+    }
+
+    @Test
+    void testsCreationOfOrbitMap() {
+        String input = "COM)B";
+
+    }
+}
